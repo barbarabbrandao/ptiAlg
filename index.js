0d337df265d1d67022a7414030f6d24a6809eedc
femmaby <83260759+femmaby@users.noreply.github.com>
2021-10-23 03:59:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var alcool, gas;
 var novo = 's';
 
 for(i=0; novo == 's'; i++){
-  alcool= prompt ("Digite o valor do litro do alcóol: ");
+  alcool= prompt ("Digite o valor do litro do álcool: ");
   gas= prompt ("Digite o valor do litro da gasolina: ");
   var comb = alcool/gas;
     if(comb > 0.7){
     alert ("Vale a pena abastecer com gasolina.");
   }else if( comb <0.7){
-    alert ("Vale a pena abastecer com alcóol.");
+    alert ("Vale a pena abastecer com álcool.");
   }else{
     alert ("Tanto faz abastecer com alcóol ou gasolina.");
   }
